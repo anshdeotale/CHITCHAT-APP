f04a3a917bf21bfeff7adabd1c458e1e27b1857e
anshdeotale <anshdeotale143@gmail.com>
2026-01-25 19:18:25 +0530

    connect with database

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import path from "path";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
@@ -19,11 +20,13 @@ if (process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname,"../frontend/vite-project/dist")))
 
     // To get react app
-    app.get("*",(req,res) => {
+    app.get("*",(_,res) => {
         res.sendFile(path.join(__dirname,"../frontend/vite-project/dist/index.html"))
     })
 }
 
 
 
-app.listen(PORT,() => {console.log('server running on port' + PORT)});
+app.listen(PORT,() => {console.log('server running on port' + PORT)
+    connectDB()
+});
