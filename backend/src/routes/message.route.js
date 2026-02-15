73ea8da47ca090493a25da19679966e741d4c3c4
anshdeotale <anshdeotale143@gmail.com>
2026-02-15 17:12:57 +0530

    message routes added

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,9 +1,17 @@
 import express from "express";
+import { protectRoute } from "../middleware/auth.middleware";
+import { getAllContaacts,getMessageByUserId,sendMessage,getChatPartners } from "../controllers/message.controller.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
 
-router.get("/send",(req,res)=>{
-    res.send("send message endpoint");
-});
+router.use(arcjetProtection,protectRoute)
+
+router.get("/contacts", getAllContaacts);
+router.get("/chats", getChatPartners);
+router.get("/:id", getMessageByUserId);
+router.post("/send/:id", sendMessage);
+
+
 
 export default router;
